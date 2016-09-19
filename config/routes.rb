d8dee422bfface6a333275ac8f7b101086456877
HamdiBayhan <hamdibayhan@gmail.com>
2016-09-19 14:21:26 +0300

    install devise gem and add devise management model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :managements
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root "members#index"
+
 end
